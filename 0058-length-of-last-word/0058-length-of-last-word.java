8ddb8f6959ffbe9c206555da5d0a7a3707085d10
Ihnatsi <120472298+IgnBys@users.noreply.github.com>
2024-08-05 19:26:45 +0200

    Time: 3 ms (5.19%), Space: 41.4 MB (75.06%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -3,17 +3,17 @@ class Solution {
         int sum = 0;
         int help_sum = 0;
         char space =' ';
-        for (char letter: s.toCharArray()) {
-            if (space==' ' && letter != space){
+        for (int i=0; i<s.length();i++) {
+            if (space==' ' && s.charAt(i) != space){
                 help_sum++;
                 if (help_sum == 1){
                     sum=0;
                 }
                 sum++;
             }
-            else if(letter == space){
+            else if(s.charAt(i) == space){
                 help_sum =0;
-                space = letter;
+                space = s.charAt(i);
             }
         }
         return sum;
